e937b44a19090a6fe5ea4e82ae4047e20b3bac86
Psevdon1m <Valentine.oleynik@gmail.com>
2023-04-07 19:27:16 +0300

    Project setup and nft mockup
    Added contract, deploy script, helper functions

---
+++ contracts/DogsCollection.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: MIT
+import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+pragma solidity ^0.8.0;
+
+contract DogsCollection is ERC721("Doggy", "DOG") {
+    uint private tokenCounter;
+    string public TOKEN_URI = "https://localhost:8080/";
+
+    function mintDog() public returns (uint256) {
+        _safeMint(msg.sender, tokenCounter);
+        return tokenCounter++;
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return tokenCounter;
+    }
+
+    function tokenURI(uint256 /* tokenId */) public view override returns (string memory) {
+        return TOKEN_URI;
+    }
+}
